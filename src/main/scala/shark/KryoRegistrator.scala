3c229f64ae080340759dbe89e8de78ee91c964f0
root <root@ip-10-120-35-107.ec2.internal>
2012-10-16 09:00:06 +0000

    Updated KryoRegistrator to reflect Kryo 2.x change.

--- src/main/scala/shark/KryoRegistrator.scala
+++ src/main/scala/shark/KryoRegistrator.scala
@@ -3,7 +3,7 @@ package shark
 import java.util.Arrays
 import java.nio.ByteBuffer
 import com.esotericsoftware.kryo.Kryo
-import com.esotericsoftware.kryo.serialize.{IntSerializer, SimpleSerializer, SerializableSerializer}
+import com.esotericsoftware.kryo.serializers.{JavaSerializer => KryoJavaSerializer}
 import de.javakaffee.kryoserializers.ArraysAsListSerializer
 import shark.execution.MapJoinOperator
 import org.apache.hadoop.hive.ql.exec.persistence.{MapJoinSingleKey, MapJoinObjectKey, MapJoinDoubleKeys, MapJoinObjectValue}
@@ -16,27 +16,11 @@ class KryoRegistrator extends spark.KryoRegistrator {
 
     // Java Arrays.asList returns an internal class that cannot be serialized
     // by default Kryo. This provides a workaround.
-    kryo.register(Arrays.asList().getClass, new ArraysAsListSerializer(kryo))
+    kryo.register(Arrays.asList().getClass, new ArraysAsListSerializer)
 
-    kryo.register(classOf[MapJoinSingleKey], new SerializableSerializer)
-    kryo.register(classOf[MapJoinObjectKey], new SerializableSerializer)
-    kryo.register(classOf[MapJoinDoubleKeys], new SerializableSerializer)
-    kryo.register(classOf[MapJoinObjectValue], new SerializableSerializer)
-
-    // This is a work-around for Kryo's byte-by-byte serialization of byte
-    // arrays. Note that it does not handle nulls because null rows are already
-    // encoded using Hive's SerDe.
-    kryo.register(classOf[Array[Byte]], new SimpleSerializer[Array[Byte]]() {
-      def write (buffer: ByteBuffer, arr: Array[Byte]) {
-        IntSerializer.put(buffer, arr.length, true)
-        buffer.put(arr)
-      }
-      def read (buffer: ByteBuffer): Array[Byte] =  {
-        val len = IntSerializer.get(buffer, true)
-        val arr = new Array[Byte](len)
-        buffer.get(arr)
-        arr
-      }
-    })
+    kryo.register(classOf[MapJoinSingleKey], new KryoJavaSerializer)
+    kryo.register(classOf[MapJoinObjectKey], new KryoJavaSerializer)
+    kryo.register(classOf[MapJoinDoubleKeys], new KryoJavaSerializer)
+    kryo.register(classOf[MapJoinObjectValue], new KryoJavaSerializer)
   }
 }
